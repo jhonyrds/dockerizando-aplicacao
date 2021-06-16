6527ecd1cf2475c6bcabe50d321c2b24574ddca1
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2021-06-16 19:31:03 -0300

    Aplicação utilizando java, springboot, postgres e docker

---
+++ src/main/java/br/com/dockerizando/dockerizando/DockerizandoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dockerizando.dockerizando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dockerizando/dockerizando/DockerizandoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dockerizando.dockerizando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
